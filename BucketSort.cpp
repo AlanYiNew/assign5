f2f575315c90a6c2b64b73f50cce08bebee53208
AlanYI New <2456556654@qq.com>
2017-10-25 19:00:05 +1100

    first commit

---
+++ BucketSort.cpp
@@ -0,0 +1,106 @@
+#include "BucketSort.h"
+
+#include <thread>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <atomic>
+#include <iostream>
+bool aLessB(const unsigned int& x, const unsigned int& y, unsigned int pow) {
+
+
+
+    if (x == y) return false; // if the two numbers are the same then one is not less than the other
+
+
+
+    unsigned int a = x;
+
+    unsigned int b = y;
+
+
+
+    // work out the digit we are currently comparing on. 
+
+    if (pow == 0) {
+
+        while (a / 10 > 0) {
+
+            a = a / 10; 
+
+        }   
+
+        while (b / 10 > 0) {
+
+            b = b / 10;
+
+        }
+
+    } else {
+
+        while (a / 10 >= (unsigned int) std::round(std::pow(10,pow))) {
+
+            a = a / 10;
+
+        }
+
+        while (b / 10 >= (unsigned int) std::round(std::pow(10,pow))) {
+
+            b = b / 10;
+
+        }
+
+    }
+
+
+    if (a == b)
+
+        return aLessB(x,y,pow + 1);  // recurse if this digit is the same 
+
+    else
+
+        return a < b;
+
+}
+
+int getMSD(unsigned int a){
+    while (a >= 10){
+        a /= 10;
+    }
+    return a;
+}
+
+
+void BucketSort::sort(unsigned int numCores) {
+
+    std::vector<std::vector<unsigned int>> numbers(9,std::vector<unsigned int>());
+
+    for ( auto k: numbersToSort){
+        numbers[getMSD(k)-1].push_back(k);
+    }
+
+
+    std::vector<std::thread> threads;
+
+    std::atomic<unsigned int> cur = ATOMIC_VAR_INIT(1);
+    for (unsigned int i = 0; i < numCores ; i++){
+        threads.push_back(std::thread([&cur,&numbers](){
+                    while (true){
+                        auto k = std::atomic_fetch_add(&cur,1U);
+                        if (k>= 10) break;        
+                        std::cout << k << std::endl; 
+                        std::sort(numbers[k-1].begin(),numbers[k-1].end(), [](const unsigned int& x, const unsigned int& y){
+
+                            return aLessB(x,y,0);
+
+                        });
+                        
+                    }
+        }));
+    }
+    for (auto & ref: threads){
+        ref.join();
+    }
+
+        
+}
---
+++ BucketSort.h
@@ -0,0 +1,23 @@
+#ifndef BUCKET_SORT_H
+
+#define BUCKET_SORT_H
+
+
+#include <vector>
+
+
+struct BucketSort {
+
+	
+
+	// vector of numbers
+
+	std::vector<unsigned int> numbersToSort;
+
+
+	void sort(unsigned int numCores);
+
+};
+
+
+#endif
