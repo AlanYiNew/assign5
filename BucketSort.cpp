c723921d57600d4bf9e1c3788f4f8d6ccf5c4c0e
alanYi <245655664@qq.com>
2017-10-25 05:52:35 -0700

    slow

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -71,36 +71,50 @@ int getMSD(unsigned int a){
 }
 
 
+
+
 void BucketSort::sort(unsigned int numCores) {
 
+        
     std::vector<std::vector<unsigned int>> numbers(9,std::vector<unsigned int>());
 
     for ( auto k: numbersToSort){
         numbers[getMSD(k)-1].push_back(k);
     }
-
+    
+    std::vector<unsigned int> count;
+    size_t temp = 0;
+    for (auto &k:numbers){
+        count.push_back(temp);
+        temp += k.size();
+    }
 
     std::vector<std::thread> threads;
-
+    
     std::atomic<unsigned int> cur = ATOMIC_VAR_INIT(1);
     for (unsigned int i = 0; i < numCores ; i++){
-        threads.push_back(std::thread([&cur,&numbers](){
+        auto lambda = [&numbers,&count,&cur,this](){
                     while (true){
                         auto k = std::atomic_fetch_add(&cur,1U);
                         if (k>= 10) break;        
-                        std::cout << k << std::endl; 
+                         
+
+                        
+
                         std::sort(numbers[k-1].begin(),numbers[k-1].end(), [](const unsigned int& x, const unsigned int& y){
 
-                            return aLessB(x,y,0);
+                            return aLessB(x,y,1);
 
                         });
                         
+                        std::copy(numbers[k-1].begin(),numbers[k-1].end(),this->numbersToSort.begin()+count[k-1]);                        
+
                     }
-        }));
+                    
+        };
+        threads.push_back(std::thread(lambda));
     }
     for (auto & ref: threads){
         ref.join();
     }
-
-        
 }
--- Makefile
+++ Makefile
@@ -3,12 +3,12 @@ all: sortTester
 
 sortTester : sortTester.o BucketSort.o
 
-	g++ -std=c++14 -Wall -Werror -O2 -g -pthread -o sortTester sortTester.o BucketSort.o
+	g++ -std=c++14 -Wall -Werror -O2 -gp -pthread -o sortTester sortTester.o BucketSort.o
 
 
 sortTester.o: sortTester.cpp BucketSort.h
 
-	g++ -std=c++14 -Wall -Werror -O2 -g -pthread -c sortTester.cpp
+	g++ -std=c++14 -Wall -Werror -O2 -gp -pthread -c sortTester.cpp
 
 
 BucketSort.o : BucketSort.h BucketSort.cpp
